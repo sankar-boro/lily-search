f35ea437e16df9503ba71799d39a30894cbdd55b
sankar.boro <sankar.boro@yahoo.com>
2022-06-15 13:38:53 +0530

    split string

--- src/file.rs
+++ src/file.rs
@@ -14,12 +14,19 @@ impl FileHandler {
 }
 
 impl FileHandler {
-    pub fn import(&mut self, file_url: &str) -> Result<FileHandler, Box<dyn std::error::Error>> {
+    pub fn read_from(&mut self, file_url: &str) -> Result<FileHandler, Box<dyn std::error::Error>> {
         let mut file = File::open(file_url)?;
         let mut contents = String::new();
         file.read_to_string(&mut contents)?;
         Ok(FileHandler {
             inner: contents
         })
+    }
+
+    pub fn get_split_string(&mut self, file_url: &str) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+        let data = self.read_from(file_url)?;
+        let data = &data.inner;
+        let split_data: Vec<String> = data.split(' ').map(|d| -> String {d.to_owned()}).collect();
+        Ok(split_data)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,7 @@ use file::*;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut file_handler = FileHandler::new();
-    let file_data = file_handler.import("lorem_ipsum.txt").unwrap();
-    let data = &file_data.inner;
-    println!("{}", data);
+    let data = file_handler.get_split_string("lorem_ipsum.txt").unwrap();
+    println!("{:?}", data);
     Ok(())    
 }
